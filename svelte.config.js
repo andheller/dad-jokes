81a4e6b1622b6d2582848ba263b42b4265764a26
aheller <aheller9@gmail.com>
2021-10-26 12:08:22 -0500

    Static Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -30,8 +30,12 @@ const config = {
 
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
-        target: "#svelte",
-        adapter: adapter(),
+        adapter: adapter({
+            // default options are shown
+            pages: "build",
+            assets: "build",
+            fallback: null,
+        }),
     },
 };
 
